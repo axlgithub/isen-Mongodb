2a19bb58254878281b76202900a0e062f8fad406
axel <axel.marlard@student.yncrea.fr>
2021-09-20 11:04:41 +0200

    improve user_programm

--- user_program.py
+++ user_program.py
@@ -1,16 +1,39 @@
 import testWriteInDb
+import os 
+
+
+
+#############################################################################
+
+###          choice of the city                                         ######
+
+#############################################################################
+
 
 def choice(i):
-    user_choice = i.lower()
-    if (user_choice == "paris"):
-        return (1)
-    if (user_choice == "lille"):
-        return (2)
-    if (user_choice == "lyon"):
-        return (3)
-    if (user_choice == "rennes1"):
-        return (4)
-    return(i)
+    user_choice = i
+    if (int(user_choice) == 1):
+        return ("paris")
+    if (int(user_choice) == 2):
+        return ("lille")
+    if (int(user_choice) == 3):
+        return ("lyon")
+    if (int(user_choice) == 4):
+        return ("rennes")
+    user_choice = user_choice.lower()
+    return(user_choice)
+
+##############################################################################
+
+
+
+
+
+#############################################################################
+
+### first_launch_from_user first message and interactions with the user #####
+
+#############################################################################
 
 def first_launch_from_user():
     print("Hello, welcome to our velib application. In which city do you want to make a research ? \n")
@@ -18,16 +41,62 @@ def first_launch_from_user():
         print("enter the name of the city or one of the number shown bellow\n ")
         print("1.Paris 2.Lille 3.Lyon 4;Rennes\n")
         user_choice = input(":")
-        number_of_choice= int(choice(user_choice))
-        if ( 1 <= number_of_choice <= 4 ):
+        city_of_user= choice(user_choice)
+        if ( city_of_user == "lille" or city_of_user == "lyon" or city_of_user == "paris" or city_of_user == "rennes"):
             break
         print("please enter a correct city or number")
-    print("ok")
-    return number_of_choice
+    return (city_of_user.capitalize())
+
+##############################################################################
+
+
+
+
+
+
+
+#############################################################################
+
+### get_user_location ask longitude and latitude and returns a list     #####
+
+#############################################################################
+
+def get_user_location():
+    L = []
+    user_longitude = input("\nplease enter your longitude: ")
+    user_latitude = input("\nplease enter your latitude: ")
+    return L[user_longitude,user_latitude]
+
+#############################################################################
+
+
+
+
+
+#############################################################################
+
+### get_closest_location make a request to find closest station         #####
+
+#############################################################################
+
+def get_closest_location(city, location_of_user):
+    return 1
+
+#############################################################################
+
 
 
 
 if __name__ == "__main__": 
     testWriteInDb.get_database("vélib")
     city_of_user = first_launch_from_user()
-    
+
+   
+    os.system("clear")
+    print("\n\n\n############################################\n\n\nWelcome to our bike programm for the city of",city_of_user)
+
+    # user location is stored as a list [longitude, latitude]
+    user_localisation = get_user_location()
+
+    print("the closest station is: ")
+    closest_location = get_closest_location(city_of_user, user_localisation)
